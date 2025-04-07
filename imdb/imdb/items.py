e324573e2dc9bb10c4d7ebfbc642d4686ddd6521
Malek BOUMEDINE <malek.boumedine@gmail.com>
2025-04-07 09:52:52 +0200

    Ajout des champs d'éléments et mise à jour du spider pour récupérer des informations détaillées sur les films depuis IMDb

--- imdb/imdb/items.py
+++ imdb/imdb/items.py
@@ -8,5 +8,22 @@ import scrapy
 
 class ImdbItem(scrapy.Item):
     # define the fields for your item here like:
-    # name = scrapy.Field()
-    pass
+    
+    original_title = scrapy.Field()
+    fr_title = scrapy.Field()
+    release_year = scrapy.Field()
+    certification = scrapy.Field()
+    duration = scrapy.Field()
+    score = scrapy.Field()
+    metascore = scrapy.Field()
+    voters_number = scrapy.Field()
+    description = scrapy.Field()
+    categories = scrapy.Field()
+    languages = scrapy.Field()
+    prod_cies = scrapy.Field()
+    countries = scrapy.Field()
+    actors = scrapy.Field()
+    directors = scrapy.Field()
+    writers = scrapy.Field()
+    budget = scrapy.Field()
+    
